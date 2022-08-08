2694539a1e1a0a2b3266e4d861905cba5e5c0334
Napervillpol <110348077+Napervillpol@users.noreply.github.com>
2022-08-08 17:02:50 -0500

    update GEOJSON

--- MN.py
+++ MN.py
@@ -21,3 +21,48 @@ DEM = df.loc[df['Candidate Name']=='Joseph R. Biden and Kamala Harris']
 df = DEM.merge(GOP, on='ID')
 df.insert(0,"Margin",(df['Percentage of votes_x'].astype(float)-df['Percentage of votes_y'].astype(float))/100)
 df.to_csv('Output.csv',index=False)
+
+
+file_name='MN_Precincts.geojson'
+
+
+with open(file_name, 'r', encoding='utf-8') as f:
+    data = json.load(f)
+
+#with urlopen("https://gis.dupageco.org/arcgis/rest/services/DuPage_County_IL/Election_Precincts/MapServer/0/query?outFields=*&where=1%3D1&f=geojson") as response:
+  # source = response.read()
+
+#data = json.loads(source)
+i = 0
+for features in data['features']:
+
+    
+    features['properties']['DEM_VOTES']= 0
+    features['properties']['GOP_VOTES']= 0
+    features['properties']['MARGIN']= 0
+    features['properties']['TOTAL_VOTES']= 0
+
+    for Precincts in df['ID']:
+
+
+        #print(features['properties']['ID'])
+
+        if features['properties']['ID'] == Precincts:
+            features['properties']['DEM_VOTES'] = int(df['Votes_x'][i])
+            features['properties']['GOP_VOTES'] = int(df['Votes_y'][i])
+            features['properties']['TOTAL_VOTES'] = int(df['Total Votes_x'][i])
+            features['properties']['MARGIN'] = float(df['Percentage of votes_x'][i])-float(df['Percentage of votes_y'][i])
+            print(df['Casten'][i])
+
+        i = i + 1
+
+    i = 0;
+
+
+
+
+#json_data = json.dumps(data)
+
+with open('TEST.geojson', 'w') as f:
+    json.dump(data, f, indent=2)
+    print("The json file is created")
