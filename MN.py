c4d22c5fdf27ff4c2e6592f6f3b6f4b5896dcd4c
Napervillpol <110348077+Napervillpol@users.noreply.github.com>
2022-08-09 13:16:57 -0500

    Testing python script

--- MN.py
+++ MN.py
@@ -12,24 +12,25 @@ def safediv(x, y):
         return 0
 def func():
 
-    df = pd.read_excel('https://ossmedia.sos.state.mn.us/MediaInfo/Congressional-District-1-Special-Election-Results.xlsx',sheet_name = 'PrecinctResults')
+    #df = pd.read_excel('https://ossmedia.sos.state.mn.us/MediaInfo/Congressional-District-1-Special-Election-Results.xlsx',sheet_name = 'PrecinctResults')
+    df = pd.read_excel(
+        'Congressional-District-1-Special-Election-Results.xlsx',
+        sheet_name='PrecinctResults')
     df.columns=df.iloc[0]
 
     df =df.drop(index =[0])
 
 
     df.insert(0, "ID",df['County Code'].astype(str)+ df['Precinct Code'].astype(str))
-    df.insert(0, 'Total',
-                    df['McClellan - GLC'] + df['Reisdorf - LMN'] + df['Finstad - R'] + df['Ettinger - DFL'] + df[
-                        'Write In'])
+    df.insert(0, 'Total', df['McClellan - GLC'] + df['Reisdorf - LMN'] + df['Finstad - R'] + df['Ettinger - DFL'] + df['Write In'])
 
-    df.insert(0,"Margin",safediv(df['Ettinger - DFL']-df['Reisdorf - LMN'],df['Total']))
+    df.insert(0,"Margin",safediv(df['Ettinger - DFL']-df['Finstad - R'],df['Total']))
 
 
 
     Counties = df.groupby(df['County Code'])[['McClellan - GLC','Reisdorf - LMN','Finstad - R','Ettinger - DFL','Write In']].sum().reset_index()
 
-    Counties.insert(0, 'Total',df['McClellan - GLC']+df['Reisdorf - LMN']+df['Finstad - R']+df['Ettinger - DFL']+df['Write In'])
+    Counties.insert(0, 'Total',Counties['McClellan - GLC']+Counties['Reisdorf - LMN']+Counties['Finstad - R']+Counties['Ettinger - DFL']+Counties['Write In'])
     Counties.insert(0,'Margin',safediv(Counties['Ettinger - DFL']-Counties['Finstad - R'], Counties['Total']))
 
     df.to_csv('Output.csv', index=False)
@@ -37,8 +38,8 @@ def func():
 
     file_name='MN_Precincts.geojson'
     df = df.dropna()
-    Counties = Counties.dropna()
-    print(df)
+    #Counties = Counties.dropna()
+
     #with open(file_name, 'r', encoding='utf-8') as f:
     #    data = json.load(f)
 
@@ -100,6 +101,7 @@ def func():
             # print(features['properties']['ID'])
 
             if features['properties']['COUN'] == Precincts:
+                print(Counties)
                 features['properties']['DEM_VOTES'] = int(Counties['Ettinger - DFL'][i])
                 features['properties']['GOP_VOTES'] = int(Counties['Finstad - R'][i])
                 features['properties']['TOTAL_VOTES'] = int(Counties['Total'][i])
